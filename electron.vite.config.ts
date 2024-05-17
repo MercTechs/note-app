3168c4c6d02ae631f86b1c46519b28cc592e2a0d
hglong16 <intihad.vuong@gmail.com>
2024-05-17 14:45:43 +0700

    chore: Update project configuration and add Tailwind CSS and PostCSS files

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,15 +4,29 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@/lib': resolve('src/main/lib'),
+        '@shared': resolve('src/shared')
+      }
+    }
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    assetsInclude: 'src/renderer/assets/**',
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared/src'),
+        '@/hooks': resolve('src/renderer/src/hooks'),
+        '@/asset': resolve('src/renderer/src/asset'),
+        '@/components': resolve('src/renderer/src/components'),
+        '@/store': resolve('src/renderer/src/store'),
+        '@/mocks': resolve('src/renderer/src/mocks'),
+        '@': resolve('src/renderer/src')
       }
     },
     plugins: [react()]
